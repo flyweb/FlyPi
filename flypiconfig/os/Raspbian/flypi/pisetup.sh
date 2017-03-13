aae99cf99cdee0253afd1ca52b7fe201c09838e7
William Qi <hello@williamqi.com>
2017-03-13 01:07:21 -0700

    add progress indicators to setup script

--- flypiconfig/os/Raspbian/flypi/pisetup.sh
+++ flypiconfig/os/Raspbian/flypi/pisetup.sh
@@ -1,14 +1,18 @@
 sudo apt-get --assume-yes update --fix-missing
+touch 1
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash - && sudo apt --assume-yes install nodejs
+touch 2
 sudo apt-get --assume-yes install build-essential libavahi-compat-libdnssd-dev tightvncserver expect
+touch 3
 
 ./spawnvnc.sh
+touch 4
 vncserver
-
-sudo mv /home/pi/rc.local /etc/rc.local
+touch 5
 
 git clone https://github.com/flyweb/FlyPi.git && cd FlyPi && git checkout feature/config
+touch 6
 sudo npm install -g node-gyp && npm install
+touch 7
 ( cd node_modules/pty.js && make clean && node-gyp configure --release && node-gyp build )
-
-sudo reboot
+touch 8
