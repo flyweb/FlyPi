9cab995feafee2c702b20e531bf30f10ae40304a
William Qi <hello@williamqi.com>
2017-03-13 02:13:43 -0700

    update config

--- flypiconfig/os/Raspbian/flypi/pisetup.sh
+++ flypiconfig/os/Raspbian/flypi/pisetup.sh
@@ -1,18 +1,18 @@
-sudo apt-get --assume-yes update --fix-missing
-touch 1
-curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash - && sudo apt --assume-yes install nodejs
-touch 2
+sudo apt-get --assume-yes update
+touch /home/pi/1
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash - && sudo apt-get --assume-yes install nodejs
+touch /home/pi/2
 sudo apt-get --assume-yes install build-essential libavahi-compat-libdnssd-dev tightvncserver expect
-touch 3
+touch /home/pi/3
 
 ./spawnvnc.sh
-touch 4
+touch /home/pi/4
 vncserver
-touch 5
+touch /home/pi/5
 
 git clone https://github.com/flyweb/FlyPi.git && cd FlyPi && git checkout feature/config
-touch 6
+touch /home/pi/6
 sudo npm install -g node-gyp && npm install
-touch 7
+touch /home/pi/7
 ( cd node_modules/pty.js && make clean && node-gyp configure --release && node-gyp build )
-touch 8
+touch /home/pi/8
